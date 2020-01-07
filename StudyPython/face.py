c62eb45a71d7292e0fd2214061f947d889c24a53
Leehogu <59024758+Leehogu@users.noreply.github.com>
2020-01-07 22:29:57 +0900

    face2

--- StudyPython/face.py
+++ StudyPython/face.py
@@ -5,7 +5,7 @@ mod = sys.modules[__name__]
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Read the input image
-img = cv2.imread('ss.jpg', cv2.IMREAD_ANYCOLOR)
+img = cv2.imread('siba.jpg', cv2.IMREAD_ANYCOLOR)
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect faces
@@ -13,11 +13,11 @@ faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 a = 0
 # Draw rectangle around the faces
 for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
     crop_img = img[y:y+h, x:x+w]
     setattr(mod, 'dst_{}'.format(a), cv2.resize(crop_img, dsize=(150, 150), interpolation=cv2.INTER_AREA))
-    cv2.imshow(str(a), getattr(mod, 'dst_{}'.format(a)))
     cv2.imwrite(str(a)+'.jpg', getattr(mod, 'dst_{}'.format(a)))
+    cv2.imshow(str(a), getattr(mod, 'dst_{}'.format(a)))
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
     a = a + 1
 # Display the output
 cv2.imshow('img', img)
