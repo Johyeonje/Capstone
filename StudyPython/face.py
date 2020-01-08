d0d8dc5a5f7e707664f6e22adfe57bc38b0b418c
Leehogu <59024758+Leehogu@users.noreply.github.com>
2020-01-08 14:16:33 +0900

    saDds

--- StudyPython/face.py
+++ StudyPython/face.py
@@ -9,14 +9,14 @@ img = cv2.imread('siba.jpg', cv2.IMREAD_ANYCOLOR)
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect faces
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+faces = face_cascade.detectMultiScale(gray, 1.05, 10)
 a = 0
 # Draw rectangle around the faces
 for (x, y, w, h) in faces:
     crop_img = img[y:y+h, x:x+w]
     setattr(mod, 'dst_{}'.format(a), cv2.resize(crop_img, dsize=(150, 150), interpolation=cv2.INTER_AREA))
-    cv2.imwrite(str(a)+'.jpg', getattr(mod, 'dst_{}'.format(a)))
-    cv2.imshow(str(a), getattr(mod, 'dst_{}'.format(a)))
+    #cv2.imwrite(str(a)+'.jpg', getattr(mod, 'dst_{}'.format(a)))
+    #cv2.imshow(str(a), getattr(mod, 'dst_{}'.format(a)))
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
     a = a + 1
 # Display the output
