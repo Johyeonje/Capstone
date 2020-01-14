c7d22f9eebbe569a334040c158b73dbdce395cc5
ShinHyoJung <59465978+ShinHyoJung@users.noreply.github.com>
2020-01-14 13:45:19 +0900

    android2

--- attendandroidapp/app/src/main/java/com/cookandroid/attendandroidapp/fragment2Activity.java
+++ attendandroidapp/app/src/main/java/com/cookandroid/attendandroidapp/fragment2Activity.java
@@ -9,13 +9,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class fragment1Activity extends Fragment {
+public class fragment2Activity extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment1, container, false);
+        return inflater.inflate(R.layout.fragment2, container, false);
     }
 }
-
 
--- attendandroidapp/app/src/main/java/com/cookandroid/attendandroidapp/recordActivity.java
+++ attendandroidapp/app/src/main/java/com/cookandroid/attendandroidapp/recordActivity.java
@@ -14,6 +14,7 @@ import android.widget.Spinner;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import java.util.ArrayList;
@@ -21,6 +22,10 @@ import java.util.ArrayList;
 public class recordActivity extends AppCompatActivity {
 
     ArrayList courseList;
+    private FragmentManager fragmentManager;
+    private fragment1Activity fragment1;
+    private fragment2Activity fragment2;
+    private FragmentTransaction transaction;
 
     final String[] select_item = {""};
 
@@ -30,20 +35,33 @@ public class recordActivity extends AppCompatActivity {
         setContentView(R.layout.record);
         setTitle("E-Attend");
 
-        courseList = new ArrayList();
+        fragmentManager = getSupportFragmentManager();
+
+        fragment1 = new fragment1Activity();
+        fragment2 = new fragment2Activity();
+
+
+        final ArrayList courseList = new ArrayList();
         courseList.add("과목1");
         courseList.add("과목2");
         courseList.add("과목3");
         courseList.add("과목4");
 
         Spinner spinner2 = findViewById(R.id.spinner2);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, courseList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item, courseList);
         spinner2.setAdapter(adapter);
 
         spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
             @Override
+
             public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                select_item[0] = String.valueOf(courseList.get(arg2));
+                transaction = fragmentManager.beginTransaction();
+                if ("과목1".equals(courseList)) {
+                    transaction.replace(R.id.frameLayout, fragment1).commitAllowingStateLoss();
+                } else if ("과목2".equals(courseList)) {
+                    transaction.replace(R.id.frameLayout, fragment2).commitAllowingStateLoss();
+                }
             }
 
             @Override
