ea22090b2602e7ea8bd55303f3d67998fe5c3937
kimdonggyufive <agyh01001@naver.com>
2020-01-17 17:49:06 +0900

    20200117
    androidk안에 new_Kone파일이 JSP와 합친 android입니다.

---
+++ androidk/new_Kone/app/src/main/java/com/example/new_kone/RequestHttpURLConnection.java
@@ -0,0 +1,75 @@
+package com.example.new_kone;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RequestHttpURLConnection {
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public String HttpURLConnection(String urlString, String params, String fileName) {
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        try {
+            FileInputStream mFileInputStream = new FileInputStream(new File(fileName));
+            URL connectUrl = new URL(urlString);
+            Log.d("Test", "mFileInputStream  is " + mFileInputStream);
+            // HttpURLConnection 통신
+            HttpURLConnection conn = (HttpURLConnection) connectUrl.openConnection();
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+            // write data
+            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + fileName + "\"" + lineEnd);
+            dos.writeBytes(lineEnd);
+            int bytesAvailable = mFileInputStream.available();
+            int maxBufferSize = 1024;
+            int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            byte[] buffer = new byte[bufferSize];
+            int bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
+            Log.d("Test", "image byte is " + bytesRead);
+            // read image
+            while (bytesRead > 0) {
+                dos.write(buffer, 0, bufferSize);
+                bytesAvailable = mFileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
+            }
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+            // close streams
+            Log.e("Test", "File is written");
+            mFileInputStream.close();
+            dos.flush();
+            // finish upload...
+            // get response
+            StringBuffer b;
+            try (InputStream is = conn.getInputStream()) {
+                b = new StringBuffer();
+                for (int ch = 0; (ch = is.read()) != -1; ) {
+                    b.append((char) ch);
+                }
+                is.close();
+            }
+            Log.e("Test", b.toString());
+            return b.toString();
+
+        } catch (Exception e) {
+            Log.d("Test", "exception " + e.getMessage());
+            return null;
+            // TODO: handle exception
+        }
+    }
+}
--- androidk/new_Kone/app/src/main/java/com/example/new_kone/take_photoF.java
+++ androidk/new_Kone/app/src/main/java/com/example/new_kone/take_photoF.java
@@ -5,24 +5,41 @@ import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class take_photoF extends Activity {
+//==================55=5=5=5=5=5============= JSP로 보내는 부분====================================규호형===============================================
 
+    private static final int REQ_CODE_SELECT_IMAGE = 100;
+    @RequiresApi(api = Build.VERSION_CODES.M)
 
+//==================================================5=5=5=5=5=5====================================규호형===============================================
 
     ImageView imageView1;
     //==========================3=3=3=3=3======변경후 필요 변수=========================
@@ -32,6 +49,7 @@ public class take_photoF extends Activity {
     Uri image_uri;
     //==============================3=3=3=3=3=3= 변경후 필요변수========================
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +59,24 @@ public class take_photoF extends Activity {
         //========================3=3=3=3=3=3=========변경후 =================================
         btn_save_photo = findViewById(R.id.CONFIRM);
         //========================3=3=3=3=3=3=========변경후 =================================
+
+        //============================5=5=5=5=5=5================jsp적용 =====================
+        Button Send_To_Jsp = (Button) findViewById(R.id.Send_To_JSP);
+
+        Send_To_Jsp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, REQ_CODE_SELECT_IMAGE);
+            }
+        });
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .permitDiskReads()
+                .permitDiskWrites()
+                .permitNetwork().build());
+        //============================================5=5=5=5=5=5=============================
 
         Button btnReturn = (Button)findViewById(R.id.back1);
         btnReturn.setOnClickListener(new View.OnClickListener() {
@@ -76,13 +112,27 @@ public class take_photoF extends Activity {
 
         // 스피너 사용 부분 ====================================4=4=4=4=4=44=========================
 
-        final String[] Class = {"과목1","과목2","과목3","과목4","과목5"};
+        final String[] Class = {"과목1","과목2","과목3","과목4","과목5"}; // 이 부분을 수정하면됨. 수정 할것은 testHere에 보관함. split를 사용하면 됨.
 
         Spinner spinner = (Spinner)findViewById(R.id.class_choose);
 
         ArrayAdapter<String> adapter;
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,Class);
         spinner.setAdapter(adapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+// i번째에 해당되는것을 선택할 수 있다.
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
         //=============================4=4=4=4=4=4=4================================================
@@ -117,10 +167,11 @@ public class take_photoF extends Activity {
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onActivityResult(int requestCode, int resultCode,Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        //===============================================================================================나의 코드
         if (resultCode == RESULT_OK) // 카메라에 기본 탑재가 되어있다. 사진을 찍고 그 사진을 사용하기를 원하면 Result_ok를 가져온다.
         {
             imageView1.setImageURI(image_uri); // 비트맵 객체를 이용해서 보여줌.
@@ -130,6 +181,144 @@ public class take_photoF extends Activity {
         {
             Toast.makeText(this,"출석체크 사진이 찍히지 않았습니다.",Toast.LENGTH_SHORT).show();
         }
+        //===============================================================================================나의 코드
+        //=================================================5=5=5=5=5=55===================================규호형===================================================
+
+        Toast.makeText(getBaseContext(), "resultCode : " + data, Toast.LENGTH_SHORT).show();
+        if (requestCode == REQ_CODE_SELECT_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    String img_path=null;
+                    img_path = getImagePathToUri(data.getData()); //이미지의 URI를 얻어 경로값으로 반환.
+
+                    DoFileUpload("http://rbghoneroom402.iptime.org:48526/JSP/Test.jsp", img_path);  //해당 함수를 통해 이미지 전송.
+                    Toast.makeText(getBaseContext(), "img_path : " + img_path, Toast.LENGTH_SHORT).show();
+                    //이미지를 비트맵형식으로 반환
+                    Bitmap image_bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                    int reWidth = (int) (getWindowManager().getDefaultDisplay().getWidth());
+                    int reHeight = (int) (getWindowManager().getDefaultDisplay().getHeight());
+
+                    //image_bitmap 으로 받아온 이미지의 사이즈를 임의적으로 조절함. width: 400 , height: 300
+                    Bitmap image_bitmap_copy = Bitmap.createScaledBitmap(image_bitmap, 400, 300, true);
+                    ImageView image = (ImageView) findViewById(R.id.imageView);  //이미지를 띄울 위젯 ID값
+                    image.setImageBitmap(image_bitmap_copy);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //=================================================5=5=5=5=5=55=5======================================규호형==========================-====================
     }
 
+    //=====================5=5=5=5=55===============jsp 연동 ======================================= 규호형================================================================
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public String HttpURLConnection(String urlString, String params, String fileName) {
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        try {
+            FileInputStream mFileInputStream = new FileInputStream(new File(fileName));
+            URL connectUrl = new URL(urlString);
+            Log.d("Test", "mFileInputStream  is " + mFileInputStream);
+            // HttpURLConnection 통신
+            HttpURLConnection conn = (HttpURLConnection) connectUrl.openConnection();
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+            // write data
+            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + fileName + "\"" + lineEnd);
+            dos.writeBytes(lineEnd);
+            int bytesAvailable = mFileInputStream.available();
+            int maxBufferSize = 1024;
+            int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            byte[] buffer = new byte[bufferSize];
+            int bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
+            Log.d("Test", "image byte is " + bytesRead);
+            // read image
+            while (bytesRead > 0) {
+                dos.write(buffer, 0, bufferSize);
+                bytesAvailable = mFileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
+            }
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+            // close streams
+            Log.e("Test", "File is written");
+            mFileInputStream.close();
+            dos.flush();
+            // finish upload...
+            // get response
+            StringBuffer b;
+            try (InputStream is = conn.getInputStream()) {
+                b = new StringBuffer();
+                for (int ch = 0; (ch = is.read()) != -1; ) {
+                    b.append((char) ch);
+                }
+                is.close();
+            }
+            Log.e("Test", b.toString());
+            return b.toString();
+
+        } catch (Exception e) {
+            Log.d("Test", "exception " + e.getMessage());
+            return null;
+            // TODO: handle exception
+        }
+    }
+
+    public String getImagePathToUri(Uri data) {
+        //사용자가 선택한 이미지의 정보를 받아옴
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(data, proj, null, null, null);
+        cursor.moveToFirst();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        //이미지의 경로 값
+        String imgPath = cursor.getString(column_index);
+        Log.d("test", imgPath);
+        return imgPath;
+    }
+
+    public void DoFileUpload(String apiUrl, String absolutePath) {
+        NetworkTask networkTask = new NetworkTask(apiUrl, absolutePath);
+        networkTask.execute();
+    }
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url, fileName;
+
+        public NetworkTask(String url, String fileName) {
+            this.url = url;
+            this.fileName = fileName;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        protected String doInBackground(Void... params) {
+            String result; // 요청 결과를 저장할 변수.
+            result = HttpURLConnection (url, "", fileName); // 해당 URL로 부터 결과물을 얻어온다.
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+            Toast.makeText(getBaseContext(), "Responce : " + s, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+    //========================5=5=5=5=55=5==========================================================규호형===============================================================
+//+여기에 JAVA:RequestHttpURLConnection을 넣기만함.
 }
