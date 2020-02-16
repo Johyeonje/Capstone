4b64f5a77f94e9db6344dec7ad44ced6f3b2abb0
ShinHyoJung <59465978+ShinHyoJung@users.noreply.github.com>
2020-02-16 11:14:14 +0900

    string2

--- attendandroidapp/app/src/main/java/com/cookandroid/attendandroidapp/attendActivity.java
+++ attendandroidapp/app/src/main/java/com/cookandroid/attendandroidapp/attendActivity.java
@@ -59,16 +59,14 @@ public class attendActivity extends AppCompatActivity {
     Button list_btn;
     ImageView image;
     Button record_btn;
-    Button send_btn;
     EditText editbox3;
-    TextView text;
+
     private static final int REQ_CODE_SELECT_IMAGE = 100;
     private final int CAMERA_CAPTURE = 111;
     private String[] data1 = {"선택하세요"};
     Uri image_uri;
     ArrayList<String> datalist;
     ArrayAdapter<String> adapter1;
-    public String data;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -81,7 +79,7 @@ public class attendActivity extends AppCompatActivity {
         Spinner spinner = findViewById(R.id.spinner1);
         editbox3 = findViewById(R.id.edibox3);
         list_btn = findViewById(R.id.list_btn);
-        text = findViewById(R.id.text);
+
 
 
         datalist = new ArrayList<String>();
@@ -139,155 +137,131 @@ public class attendActivity extends AppCompatActivity {
                 .permitNetwork().build());
     }
 
-    public class NetworkTask extends AsyncTask<Void, Void, String> {
 
-        private String url, fileName;
-
-        public NetworkTask(String url, String fileName) {
-            this.url = url;
-            this.fileName = fileName;
+        private void checkSelfPermission() {
+            String temp = "";
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+            } //파일 쓰기 권한 확인
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+            }
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+                temp += Manifest.permission.INTERNET + " ";
+            }
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                temp += Manifest.permission.CAMERA + " ";
+            }
+            if (TextUtils.isEmpty(temp) == false) { // 권한 요청
+                ActivityCompat.requestPermissions(this, temp.trim().split(" "), 1);
+            }
         }
 
         @Override
-        protected String doInBackground(Void... params) {
-            String result; // 요청 결과를 저장할 변수.
-            result = HttpURLConnection(url, "", fileName); // 해당 URL로 부터 결과물을 얻어온다.
-            return result;
-        }
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            Toast.makeText(getBaseContext(), "resultCode : " + data, Toast.LENGTH_SHORT).show();
+            if (requestCode == REQ_CODE_SELECT_IMAGE) {
+                if (resultCode == Activity.RESULT_OK) {
+                    try {
+                        String img_path = null;
+                        img_path = getImagePathToUri(data.getData()); //이미지의 URI를 얻어 경로값으로 반환.
 
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
-            Toast.makeText(getBaseContext(), "Responce : " + s, Toast.LENGTH_SHORT).show();
-            text.setText(s);
-            data = text.getText().toString();
-        }
-    }
+                        DoFileUpload("http://rbghoneroom402.iptime.org:48526/JSP/Test.jsp", img_path);  //해당 함수를 통해 이미지 전송.
+                        //이미지를 비트맵형식으로 반환
+                        Bitmap image_bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                        int reWidth = (int) (getWindowManager().getDefaultDisplay().getWidth());
+                        int reHeight = (int) (getWindowManager().getDefaultDisplay().getHeight());
 
-
-    private void checkSelfPermission() {
-        String temp = "";
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
-        } //파일 쓰기 권한 확인
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
-        }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
-            temp += Manifest.permission.INTERNET + " ";
-        }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            temp += Manifest.permission.CAMERA + " ";
-        }
-        if (TextUtils.isEmpty(temp) == false) { // 권한 요청
-            ActivityCompat.requestPermissions(this, temp.trim().split(" "), 1);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Toast.makeText(getBaseContext(), "resultCode : " + data, Toast.LENGTH_SHORT).show();
-        if (requestCode == REQ_CODE_SELECT_IMAGE) {
-            if (resultCode == Activity.RESULT_OK) {
-                try {
-                    String img_path = null;
-                    img_path = getImagePathToUri(data.getData()); //이미지의 URI를 얻어 경로값으로 반환.
-
-                    DoFileUpload("http://rbghoneroom402.iptime.org:48526/JSP/Test.jsp", img_path);  //해당 함수를 통해 이미지 전송.
-                    //이미지를 비트맵형식으로 반환
-                    Bitmap image_bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
-                    int reWidth = (int) (getWindowManager().getDefaultDisplay().getWidth());
-                    int reHeight = (int) (getWindowManager().getDefaultDisplay().getHeight());
-
-                    //image_bitmap 으로 받아온 이미지의 사이즈를 임의적으로 조절함. width: 400 , height: 300
-                    Bitmap image_bitmap_copy = Bitmap.createScaledBitmap(image_bitmap, 400, 300, true);
-                    ImageView image = (ImageView) findViewById(R.id.image);  //이미지를 띄울 위젯 ID값
-                    image.setImageBitmap(image_bitmap_copy);
-                } catch (Exception e) {
-                    e.printStackTrace();
+                        //image_bitmap 으로 받아온 이미지의 사이즈를 임의적으로 조절함. width: 400 , height: 300
+                        Bitmap image_bitmap_copy = Bitmap.createScaledBitmap(image_bitmap, 400, 300, true);
+                        ImageView image = (ImageView) findViewById(R.id.image);  //이미지를 띄울 위젯 ID값
+                        image.setImageBitmap(image_bitmap_copy);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
+            super.onActivityResult(requestCode, resultCode, data);
         }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
 
-    public String getImagePathToUri(Uri data) {
-        //사용자가 선택한 이미지의 정보를 받아옴
-        String[] proj = {MediaStore.Images.Media.DATA};
-        Cursor cursor = managedQuery(data, proj, null, null, null);
-        cursor.moveToFirst();
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        //이미지의 경로 값
-        String imgPath = cursor.getString(column_index);
-        Log.d("test", imgPath);
-        return imgPath;
-    }
+        public String getImagePathToUri(Uri data) {
+            //사용자가 선택한 이미지의 정보를 받아옴
+            String[] proj = {MediaStore.Images.Media.DATA};
+            Cursor cursor = managedQuery(data, proj, null, null, null);
+            cursor.moveToFirst();
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            //이미지의 경로 값
+            String imgPath = cursor.getString(column_index);
+            Log.d("test", imgPath);
+            return imgPath;
+        }
 
-    public void DoFileUpload(String apiUrl, String absolutePath) {
-        NetworkTask networkTask = new NetworkTask(apiUrl, absolutePath);
-        networkTask.execute();
-    }
+        public void DoFileUpload(String apiUrl, String absolutePath) {
+            fragment1Activity.NetworkTask networkTask = new fragment1Activity.NetworkTask(apiUrl,absolutePath);
+            networkTask.execute();
+        }
 
-    public String HttpURLConnection(String urlString, String params, String fileName) {
-        String lineEnd = "\r\n";
-        String twoHyphens = "--";
-        String boundary = "*****";
-        try {
-            FileInputStream mFileInputStream = new FileInputStream(new File(fileName));
-            URL connectUrl = new URL(urlString);
-            Log.d("Test", "mFileInputStream  is " + mFileInputStream);
-            // HttpURLConnection 통신
-            HttpURLConnection conn = (HttpURLConnection) connectUrl.openConnection();
-            conn.setDoInput(true);
-            conn.setDoOutput(true);
-            conn.setUseCaches(false);
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Connection", "Keep-Alive");
-            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-            // write data
-            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
-            dos.writeBytes(twoHyphens + boundary + lineEnd);
-            dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + fileName + "\"" + lineEnd);
-            dos.writeBytes(lineEnd);
-            int bytesAvailable = mFileInputStream.available();
-            int maxBufferSize = 1024;
-            int bufferSize = Math.min(bytesAvailable, maxBufferSize);
-            byte[] buffer = new byte[bufferSize];
-            int bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
-            Log.d("Test", "image byte is " + bytesRead);
-            // read image
-            while (bytesRead > 0) {
-                dos.write(buffer, 0, bufferSize);
-                bytesAvailable = mFileInputStream.available();
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
-            }
-            dos.writeBytes(lineEnd);
-            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-            // close streams
-            Log.e("Test", "File is written");
-            mFileInputStream.close();
-            dos.flush();
-            // finish upload...
-            // get response
-            StringBuffer b;
-            try (InputStream is = conn.getInputStream()) {
-                b = new StringBuffer();
-                for (int ch = 0; (ch = is.read()) != -1; ) {
-                    b.append((char) ch);
+
+        public static String HttpURLConnection(String urlString, String params, String fileName) {
+            String lineEnd = "\r\n";
+            String twoHyphens = "--";
+            String boundary = "*****";
+            try {
+                FileInputStream mFileInputStream = new FileInputStream(new File(fileName));
+                URL connectUrl = new URL(urlString);
+                Log.d("Test", "mFileInputStream  is " + mFileInputStream);
+                // HttpURLConnection 통신
+                HttpURLConnection conn = (HttpURLConnection) connectUrl.openConnection();
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setUseCaches(false);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                // write data
+                DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + fileName + "\"" + lineEnd);
+                dos.writeBytes(lineEnd);
+                int bytesAvailable = mFileInputStream.available();
+                int maxBufferSize = 1024;
+                int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                byte[] buffer = new byte[bufferSize];
+                int bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
+                Log.d("Test", "image byte is " + bytesRead);
+                // read image
+                while (bytesRead > 0) {
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = mFileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
                 }
-                is.close();
-            }
-            Log.e("Test", b.toString());
-            return b.toString();
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+                // close streams
+                Log.e("Test", "File is written");
+                mFileInputStream.close();
+                dos.flush();
+                // finish upload...
+                // get response
+                StringBuffer b;
+                try (InputStream is = conn.getInputStream()) {
+                    b = new StringBuffer();
+                    for (int ch = 0; (ch = is.read()) != -1; ) {
+                        b.append((char) ch);
+                    }
+                    is.close();
+                }
+                Log.e("Test", b.toString());
+                return b.toString();
 
-        } catch (Exception e) {
-            Log.d("Test", "exception " + e.getMessage());
-            return null;
-            // TODO: handle exception
+            } catch (Exception e) {
+                Log.d("Test", "exception " + e.getMessage());
+                return null;
+                // TODO: handle exception
+            }
         }
-    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
--- attendandroidapp/app/src/main/java/com/cookandroid/attendandroidapp/fragment1Activity.java
+++ attendandroidapp/app/src/main/java/com/cookandroid/attendandroidapp/fragment1Activity.java
@@ -1,5 +1,6 @@
 package com.cookandroid.attendandroidapp;
 
+import android.app.Notification;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.textclassifier.TextClassification;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -23,13 +25,15 @@ import androidx.fragment.app.Fragment;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.CollationElementIterator;
 import java.util.ArrayList;
 
 public class fragment1Activity extends Fragment {
-
+  
     public fragment1Activity() {
     }
 
@@ -39,19 +43,19 @@ public class fragment1Activity extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
+   
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.fragment1, container, false);
         Button search_btn = linearLayout.findViewById(R.id.check_btn);
         final TextView text = linearLayout.findViewById(R.id.text);
-        int nDatCnt=0;
+        int nDatCnt = 0;
         ArrayList<itemActivity> oData = new ArrayList<>();
-        final String[] data = {"000001","000002","000003","000004","000005","000006"};
+        final String[] data = {"000001", "000002", "000003", "000004", "000005", "000006"};
 
-        for (int i=0; i<data.length; ++i)
-        {
+
+        for (int i = 0; i < data.length; ++i) {
             itemActivity oItem = new itemActivity();
             oItem.data = data[nDatCnt++];
             oData.add(oItem);
@@ -67,35 +71,67 @@ public class fragment1Activity extends Fragment {
         search_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            switch(text.getText().toString()) {
+                switch (text.getText().toString()) {
 
-                case "000001":
-                    list1.setItemChecked(0,true);
-                    break;
-                case "000002":
-                    list1.setItemChecked(1,true);
-                    break;
-                case "000003":
-                    list1.setItemChecked(2,true);
-                    break;
-                case "000004":
-                    list1.setItemChecked(3,true);
-                    break;
-                case "000005":
-                    list1.setItemChecked(4,true);
-                    break;
-                case "000006":
-                    list1.setItemChecked(5,true);
-                    break;
+                    case "000001":
+                        list1.setItemChecked(0, true);
+                        break;
+                    case "000002":
+                        list1.setItemChecked(1, true);
+                        break;
+                    case "000003":
+                        list1.setItemChecked(2, true);
+                        break;
+                    case "000004":
+                        list1.setItemChecked(3, true);
+                        break;
+                    case "000005":
+                        list1.setItemChecked(4, true);
+                        break;
+                    case "000006":
+                        list1.setItemChecked(5, true);
+                        break;
 
+                }
             }
-            }
-            });
+        });
 
-     return linearLayout;
+        return linearLayout;
+    }
+
+    public static class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url, fileName;
+        private CollationElementIterator text;
+
+
+        public NetworkTask(String url, String fileName) {
+            this.url = url;
+            this.fileName = fileName;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String result; // 요청 결과를 저장할 변수.
+            result = attendActivity.HttpURLConnection(url, "", fileName);
+            return result;
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+            Toast.makeText(getApplicationContext(),"response:"+s,Toast.LENGTH_SHORT);
+            text.setText(s);
+        }
+
+        private Context getApplicationContext() {
+            return getApplicationContext();
+        }
+    }
+
     }
 
 
-
-}
 
