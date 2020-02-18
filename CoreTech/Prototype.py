2ed7ea1df2be219385a5d87317d83d96a82fb660
Leehogu <rbgh1995@naver.com>
2020-02-18 17:43:51 +0900

    중복 쓰레기 삭제

---
+++ CoreTech/Prototype.py
@@ -0,0 +1,103 @@
+import sys
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import cv2
+import numpy as np
+import tensorflow as tf
+import dlib
+import glob
+import openface
+
+
+def load_image(file_name, mode=cv2.IMREAD_ANYCOLOR):
+    img = cv2.imread(file_name, mode)
+    return img
+
+
+def make_x_y(input_list, cmp_list, dtype=np.float32):
+    x = []
+    _x = []
+    num_list = []
+    for i, cmp_img in enumerate(cmp_list):
+        for j, input_img in enumerate(input_list):
+            _x = cv2.hconcat([cmp_img, input_img])
+            x.append(_x)
+            num_list.append(cmp_stu_list[i])
+    return np.array(x).astype(dtype), num_list
+
+
+def build_model():
+    # CNN
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(8, (2, 2), activation='relu', padding='same', input_shape=(200, 400, 3)))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Conv2D(16, (2, 2), activation='relu', padding='same'))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Dropout(0.25))
+    model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+
+    # 출력층(Dense) 추가
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(64, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.5))
+    model.add(tf.keras.layers.Dense(2, activation='softmax'))
+
+    return model
+
+
+if __name__ == "__main__":
+    # file_path = sys.argv[1]
+    # file_name = sys.argv[2]
+    file_path = "D:/Study/Capstone/CoreTech"
+    file_name = "1579970655152-0.jpg"
+    predictor_model = os.path.join(file_path, "../../FaceDataSet/shape_predictor_68_face_landmarks.dat")
+    cmp_img_path = os.path.join(file_path, "RealTest/CmpImg")
+    org_img_path = file_path + "/" + file_name
+    model_name = os.path.join(file_path, "../../FaceDataSet/trained_model8/model-epoch-5000")
+    cmp_stu_list = []
+    cmp_data_list = []
+    cmp_img_list = []
+    input_data_list = []
+    cmp_num = 0
+    trigger = 0
+    cmp_stu_list = glob.glob(cmp_img_path + "/*.jpg")
+    for i, cmp_stu in enumerate(cmp_stu_list):
+        cmp_img = load_image(cmp_stu)
+        cmp_data_list.append(cmp_img)
+    org_img = load_image(org_img_path)
+    face_detector = dlib.get_frontal_face_detector()
+    face_pose_predictor = dlib.shape_predictor(predictor_model)
+    face_aligner = openface.AlignDlib(predictor_model)
+    detected_faces = face_detector(org_img, 1)
+    for j, face_rect in enumerate(detected_faces):
+        trigger = 1
+        left, right, top, bottom = face_rect.left(), face_rect.right(), face_rect.top(), face_rect.bottom()
+        try:
+            pose_landmarks = face_pose_predictor(org_img, face_rect)
+            alignedFace = face_aligner.align(200, org_img, face_rect, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+            input_data_list.append(alignedFace)
+        except Exception as ex:
+            print(ex)
+    if trigger == 0:
+        os.remove(org_img_path)
+        print("얼굴 못 찾음")
+        exit()
+    input_img_list = np.array(input_data_list)
+    cmp_img_list = np.array(cmp_data_list)
+    cat_set, num_list = make_x_y(input_img_list, cmp_img_list)
+    model = build_model()
+    optimizer = tf.keras.optimizers.Adam()
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    model.load_weights(model_name)
+    prediction = model.predict(cat_set)
+    k = None
+    for i, compare in enumerate(prediction):
+        if prediction[i][0] > cmp_num:
+            if k != num_list[i]:
+                print(num_list[i])
+                k = num_list[i]
+    os.remove(org_img_path)
---
+++ CoreTech/eclipse/JSP/WebContent/Test.jsp
@@ -0,0 +1,71 @@
+<%@page import="java.io.File"%>
+<%@page import="java.io.IOException"%>
+<%@page import="java.util.Enumeration"%>
+<%@page import="com.oreilly.servlet.multipart.DefaultFileRenamePolicy"%>
+<%@page import="com.oreilly.servlet.MultipartRequest"%>
+<%@page import="java.io.OutputStream"%>
+<%@page import="java.io.DataOutputStream"%>
+<%@page import="java.sql.*"%>
+<%@page import="java.io.BufferedReader"%>
+<%@page import="java.io.InputStreamReader"%>
+<%@page import="java.text.SimpleDateFormat"%>
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<%
+		//이미지를 저장할 경로 입력.
+		request.setCharacterEncoding("utf-8");
+		
+		String folderTypePath = "D:/Study/Capstone/CoreTech";
+		String name = new String();
+		String fileName = new String();
+		int sizeLimit = 20 * 1024 * 1024; // 5메가까지 제한 넘어서면 예외발생
+		try {
+			long time = System.currentTimeMillis(); 
+			SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+			String str = dayTime.format(new Date(time));
+			System.out.println("연결시작 "+str);
+			MultipartRequest multi = new MultipartRequest(request, folderTypePath, sizeLimit, new DefaultFileRenamePolicy());
+			Enumeration files = multi.getFileNames();
+
+			//파일 정보가 있다면
+			if (files.hasMoreElements()) {
+				name = (String) files.nextElement();
+				fileName = multi.getFilesystemName(name);
+			}
+			System.out.println("이미지를 저장하였습니다. : " + fileName);
+			out.clear();
+			out = pageContext.pushBody();
+			OutputStream outputStream = response.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(outputStream);
+			Runtime runtime = Runtime.getRuntime();
+			Process process = runtime.exec("conda run -n tf python "+folderTypePath+"/Prototype.py "+folderTypePath+" "+fileName);
+			process.waitFor();
+			BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String s;
+			while ((s = br.readLine())!= null) {
+				if (s.length()>10) {
+					dos.writeUTF(s.substring(s.length()-10, s.length()-4));
+					System.out.println(s.substring(s.length()-10, s.length()-4));
+				} else if(s.length() != 0){
+					dos.writeUTF(s);
+					System.out.println(s);
+				}
+			}
+			dos.close();
+			outputStream.close();
+			br.close();
+			process.destroy();
+		} catch (IOException e) {
+			System.out.println("실패!");
+		}
+	%>
+
+</body>
+</html>
