b3ea2f24f29bd2b270cda13e1c823dcf67fca496
kimdonggyufive <agyh01001@naver.com>
2020-04-10 15:20:40 +0900

    2020/04/10
    How_to_Send_String

---
+++ Android/Android_KIM/Android_Function/app.How_to_use_String/string/app/src/main/java/com/example/myapplication/android_to_jsp.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import java.io.DataInputStream;
+import java.io.InputStream;
+
+public class android_to_jsp {
+
+
+
+
+
+
+
+    // jsp에서 text를 받을때.
+/*
+    request.setCharacterEncoding("utf-8"); // 다음 utf8 형식으로 요청을 한다.
+        try {
+        System.out.println("text시작");
+        StringBuffer b;
+        InputStream inputStream = request.getInputStream();
+        DataInputStream dis = new DataInputStream(inputStream);
+        b = new StringBuffer();
+        for (String ch; (ch = dis.readUTF()) != null;)  {
+            b.append(ch);
+        }
+        System.out.print(b.toString());
+        inputStream.close();
+    } catch (Exception e) {
+        System.out.println("실패!");
+    }
+
+ */
+}
