2502716640d141684d2db8884e50e24d0f487b4f
Johyeonje <59463494+Johyeonje@users.noreply.github.com>
2020-05-20 16:19:43 +0900

    Update main.py

--- PyAdsChange/main.py
+++ PyAdsChange/main.py
@@ -18,7 +18,9 @@ def load_image(filename):
     img /= 255.0
     return img
 
-def make_batch(img):
+def make_batch():
+    x = []
+    y = []
     batch_num = 10
     data_path = "D:\\"      #data path
     age_list = os.listdir(data_path)
@@ -26,7 +28,7 @@ def make_batch(img):
         id = random.choice(age_list)            # choice age
         id_path = os.path.join(data_path, id)
         image_list = os.listdir(id_path)        # choice image
-        image_id = random.chice(image_list)
+        image_id = random.choice(image_list)
         image_path = os.path.join(id_path, image_id)
         image = load_image(image_path)          # load image
         x.append(image)
