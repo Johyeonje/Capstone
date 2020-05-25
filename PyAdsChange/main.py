eee7c7170c251b4efe469c3c5f8810311d028480
Johyeonje <59463494+Johyeonje@users.noreply.github.com>
2020-05-25 15:16:50 +0900

    Update main.py
    update algorism for make batch

--- PyAdsChange/main.py
+++ PyAdsChange/main.py
@@ -12,6 +12,7 @@ width = 100
 height = 100
 n_channels = 3
 
+
 def load_data(path):
     data = dict()
     id_list = os.listdir(path)
@@ -21,36 +22,33 @@ def load_data(path):
 
     return data
 
+
 def load_image(filename):
     img = cv2.imread(filename, flags=cv2.IMREAD_COLOR).astype(np.float32)
     img /= 255.0
     return img
 
+
 def make_batch(id_list, data, dtype=np.float32):
     x = list()
     y = list()
-    batch_num = 10
-    age_list = os.listdir(data_path)
+    batch_num = 50
     for i in range(batch_num):
-        id = random.choice(age_list)            # choice age
-        id_path = os.path.join(data_path, id)
-        image_list = os.listdir(id_path)        # choice image
-        image_id = random.choice(image_list)
-        image_path = os.path.join(id_path, image_id)
-        image = load_image(image_path)          # load image
+        # make batch x
+        id = random.choice(id_list)            # choice age
+        img_pick = random.choice(data[id])
+        x.append(img_pick)
 
         # make label
-        label = np.zeros(shape=[12,1], dtype=np.float32)
+        label = np.zeros(shape=[10,1], dtype=np.float32)
         label[(int(id)-1),0] = 1
         print(y)
 
-        x.append(image)
-        y.append(label)
-
-        print(x.shape)
-        print(y.shape)
+    print(x.shape)
+    print(y.shape)
 
     return np.array(x).astype(dtype), np.array(y).astype(dtype)
+
 
 if __name__ == "__main__":
     data_path = "D:/Study/All-Age-Faces/F_crop"      #data path
